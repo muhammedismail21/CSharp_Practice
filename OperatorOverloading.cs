78fc384192acf3f97fe1edc6e117e1a94c0e81e6
Mohammed Ismail <muhammed.ismail21@gmail.com>
2017-06-10 15:34:49 -0500

    Method overloading and virtual methods
    Polymorphism examples

---
+++ OperatorOverloading.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace OOPs
+{
+    class OperatorOverloading
+    {
+
+
+        public static void Main()
+        {
+            /*
+            int num1 = 10;
+            int num2 = 20;
+            int res = num1 + num2;            
+            Console.WriteLine("Sum: " + res);
+            */
+            Number num1 = new Number();
+            num1.x = 1;
+            num1.y = 2;
+            Number num2 = new Number();
+            num2.x = 2;
+            num2.y = 1;
+            Number res = new Number();
+
+            res = num1 + num2;
+            Console.WriteLine("res.x= " + res.x + " " + "res.y= " + res.y);
+            Console.ReadLine();
+        }
+    }
+
+    class Number
+    {
+        public int x;
+        public int y;
+        public static Number operator +(Number num1, Number num2)
+        {
+            Number res = new Number();
+            res.x = num1.x + num2.x;
+            res.y = num1.y + num2.y;
+
+            return res;
+        }
+    }
+}
---
+++ VirtualMethodDemo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace OOPs
+{
+    class VirtualMethodDemo
+    {
+        static void Main()
+        {
+            //Employee emp = new Employee(101, "scott allen", 5000);
+            //emp.Display(ConsoleColor.Magenta);
+            //Manager manager = new Manager(102, "ruth peter", 4000, 1);
+            //manager.Display(ConsoleColor.Green);
+
+            Employee emp = new Manager(102, "scott", 4000, 1);
+            emp.Display(ConsoleColor.Cyan);
+
+            emp = new Employee(101, "allen", 5000);
+            emp.Display(ConsoleColor.DarkGreen);
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class Employee
+    {
+       protected int empID;
+       protected string empName;
+       protected double salary;
+        public Employee(int empID, string empName, double salary)
+        {
+            this.empID = empID;
+            this.empName = empName;
+            this.salary = salary;
+        }
+        public virtual void Display(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine("Emp ID: " + empID);
+            Console.WriteLine("Emp Name: " + empName);
+            Console.WriteLine("Salary: " + salary);
+            Console.ResetColor();
+        }
+    }
+    class Manager : Employee
+    {
+        int managerID;
+        public Manager(int empID, string empName, double salary, int managerID)
+            :base(empID, empName, salary)
+        {
+            this.managerID = managerID;
+        }
+        public override void Display(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine("Manager ID: " + managerID);
+            Console.WriteLine("Emp ID: " + empID);
+            Console.WriteLine("Emp Name: " + empName);
+            Console.WriteLine("Salary: " + salary);
+            Console.ResetColor();
+        }
+    }
+}
