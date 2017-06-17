fe3e8cc10304d049cee6877e8f851c0c306a2905
Mohammed Ismail <muhammed.ismail21@gmail.com>
2017-06-17 15:18:35 -0500

    first commit
    Abstract clalss and abstract method demo.

---
+++ InterfaceDemo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace OOPs
+{
+    class InterfaceDemo
+    {
+        public static void Main()
+        {
+            ICalc calc = new Calculator();
+            IMath math = new Calculator();
+
+            Console.WriteLine("Squareroot: " + calc.Squareroot(5));
+            Console.WriteLine("5 power 3: " + calc.XpowerY(5,3));
+            Console.WriteLine("Sum: " + math.Add(1,2));
+            Console.WriteLine("Product: " + math.Mul(3,3));
+            Console.ReadLine();
+
+        }
+    }
+
+    interface ICalc
+    {
+        //by default the methods in interface are public and abstract
+        double Squareroot(int number);
+        double XpowerY(int x, int y);
+    }
+    interface IMath
+    {
+        int Add(int x, int y);
+        int Mul(int x, int y);
+    }
+    class Calculator : ICalc, IMath
+    {
+        public int Add(int x, int y)
+        {
+            return (x + y);
+        }
+
+        public int Mul(int x, int y)
+        {
+            return (x * y);
+        }
+
+        public double Squareroot(int number)
+        {
+            return Math.Sqrt(number);
+        }
+
+        public double XpowerY(int x, int y)
+        {
+            return Math.Pow(x, y);
+        }
+    }
+
+}
