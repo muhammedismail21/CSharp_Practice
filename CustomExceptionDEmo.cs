b5b0e94717c3e9e12e5af19c602da9798f569fa1
Mohammed Ismail <muhammed.ismail21@gmail.com>
2017-07-01 16:04:42 -0500

    CustomExcetion
    A sample example on User defined exception or custom exception.

---
+++ CustomExceptionDEmo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class CustomExceptionDemo
+    {
+        public static void Main()
+        { 
+            Calculator calc = new Calculator();
+            int num = -12;
+            int res = 0;
+            try
+            {
+                calc.Factorial(num);
+            }
+            catch (SignedNumberException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: " + ex.Message);
+                Console.ResetColor();
+            }
+            Console.WriteLine("Factorial of {0} is: {1}", num, res);
+
+            Console.ReadLine();
+        }
+    }
+
+    class SignedNumberException : ApplicationException
+    {
+        public SignedNumberException(string errorMessage) : base(errorMessage) { }
+    }
+
+    class Calculator
+    {
+        public int Factorial(int num)
+        {
+            int fact = 1;
+
+            if (num < 0)
+            {
+                throw new SignedNumberException("Number cannot be -ve.");
+            }
+
+            for (int i = 1 ; i <= num; i++)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+    }
+
+}
