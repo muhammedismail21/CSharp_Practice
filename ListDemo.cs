84f7da04297226f9ca36c84053c4c78930e3d8ee
Mohammed Ismail <muhammed.ismail21@gmail.com>
2017-07-08 15:17:22 -0500

    first commit
    Collections and Generic Collections examples

---
+++ ListDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsProject
+{
+    class ListDemo
+    {
+        public static void Main()
+        {
+            List<int> rankList = new List<int>();
+            rankList.Add(21);
+            rankList.Add(35);
+            rankList.Add(43);
+            rankList.Add(79);
+            rankList.Add(15);
+
+            Console.Write("List elements: ");
+            foreach (int item in rankList)
+            {
+                Console.Write(item + " ");
+            }
+
+            rankList.Sort();
+
+            Console.Write("\nList elements: ");
+            foreach (int item in rankList)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,55 @@
 ﻿using System;
+using System.Collections;
 
-namespace ConsoleApplication1
+namespace CollectionsProject
 {
-    class Sample
-    {
-        public Sample()
-        {
-            Console.WriteLine("Sample() ...");
-        }
-        public Sample(string name)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Hi, " + name + "!");
-            Console.ResetColor();
-        }
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Sample obj = new Sample();
-            Sample obj2 = new Sample("Mohammed");
+            ArrayList alist = new ArrayList();
+
+            Console.WriteLine("No of elements: " + alist.Count);
+            Console.WriteLine("Capacity: " + alist.Capacity);
+
+            alist.Add("one");
+            alist.Add("two");
+
+            Console.WriteLine("\nElements: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < alist.Count; i++)
+            {
+                Console.WriteLine(alist[i]);
+            }
+            Console.ResetColor();
+            Console.WriteLine("No of elements: " + alist.Count);
+            Console.WriteLine("Capacity: " + alist.Capacity);
+
+
+            alist.Add("three");
+            alist.Add("four");
+
+            Console.WriteLine("\nElements: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < alist.Count; i++)
+            {
+                Console.WriteLine(alist[i]);
+            }
+            Console.ResetColor();
+            Console.WriteLine("No of elements: " + alist.Count);
+            Console.WriteLine("Capacity: " + alist.Capacity);
+
+            alist.Add("five");
+
+            Console.WriteLine("\nElements: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < alist.Count; i++)
+            {
+                Console.WriteLine(alist[i]);
+            }
+            Console.ResetColor();
+            Console.WriteLine("No of elements: " + alist.Count);
+            Console.WriteLine("Capacity: " + alist.Capacity);
 
             Console.ReadLine();
         }
